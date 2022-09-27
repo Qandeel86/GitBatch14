a07e73261dd87a64fb5bebb206824cec1a955089
Qandeel86 <qandeel.wasim@gmail.com>
2022-09-27 19:52:13 -0400

    second commit

--- src/class03B14/Class03.java
+++ src/class03B14/Class03.java
@@ -3,8 +3,8 @@ package class03B14;
 public class Class03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hey this is a git class");
+		System.out.println("Hey this is a change in my project");
 	}
 
 }
